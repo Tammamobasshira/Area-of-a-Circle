7c940892acaac962a06c12054329738e84c89101
Tamma Mobasshira <96113283+Tammamobasshira@users.noreply.github.com>
2022-02-20 20:51:47 +0600

    Add files via upload

---
+++ 1. The Area of the Circle.c
@@ -0,0 +1,16 @@
+
+/*Area of the Circle
+  Circumference of the Circle*/
+#include <stdio.h>
+#define pi 3.1416
+void main()
+
+    {
+        float radius, area, circumference;
+            printf ("Enter Your Radius Value: ");
+        scanf ("%f", & radius);
+        area = pi * radius * radius;
+        circumference = 2 * pi * radius;
+        printf ("Area of the Circle is .2%f\n", area);
+        printf ("Area of the Circumference is .2%f\n", circumference);
+    }
